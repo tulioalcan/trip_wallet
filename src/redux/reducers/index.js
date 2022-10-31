c91f6ae1bc6dab750fdf4bd6f982f30ee6500425
Túlio Mendes <tulioalcan@gmail.com>
2022-10-31 15:47:59 -0300

    configurando redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,22 @@
-// import user from './user';
-// import wallet from './wallet';
+import { combineReducers } from 'redux';
+import user from './user';
+import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const INITIAL_STATE = {
+  user,
+  wallet,
+};
+
+const walletReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+const rootReducer = combineReducers({ walletReducer });
+
+export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
 // configure aqui sua store
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { legacy_createStore as createStore } from 'redux';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
