bb894ec9684bed5a47e71b4aa92a40929d3a614a
Túlio Mendes <tulioalcan@gmail.com>
2022-11-01 15:31:29 -0300

    requisito 02

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <Header />
+    );
   }
 }
 
-export default Wallet;
+export default connect()(Wallet);
