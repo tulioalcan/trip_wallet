af342fe80ece31ec5156ff0aabeaf06e28a6e720
Túlio Mendes <tulioalcan@gmail.com>
2022-11-01 16:17:30 -0300

    desenvolvendo req 03

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,32 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <>
+        <div>WalletForm</div>
+        <label htmlFor="value">
+          <input data-testid="value-input" />
+        </label>
+        <label htmlFor="description">
+          <input data-testid="description-input" />
+        </label>
+        <label htmlFor="slects">
+          <select data-testid="currency-input">
+            {/* <option key={ coin } /> */}
+          </select>
+          <select data-testid="method-input">
+            <option value="dinheiro">Dinheiro</option>
+            <option value="credito">Cartão de crédito</option>
+            <option value="debito">Cartão de débito</option>
+          </select>
+          <select data-testid="tag-input">
+            <option value="alimentação">Alimentação</option>
+            <option value="lazer">Lazer</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="transporte">Transporte</option>
+            <option value="saude">Saúde</option>
+          </select>
+        </label>
+      </>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <WalletForm />
+      </>
     );
   }
 }
