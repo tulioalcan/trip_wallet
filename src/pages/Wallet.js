6f56890493ae23723b5b88803e87a3838c2a16a2
Túlio Mendes <tulioalcan@gmail.com>
2022-11-04 14:22:19 -0300

    requisito 06

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
@@ -9,6 +10,7 @@ class Wallet extends React.Component {
       <>
         <Header />
         <WalletForm />
+        <Table />
       </>
     );
   }
